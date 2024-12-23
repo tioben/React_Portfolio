f3fa436ac1dd72508d07a23d992aef9017294964
Gustavo Martins <tioben@outlook.com.br>
2024-12-23 12:48:05 -0300

    23-12-2024

--- src/pages/Home/sections/Hero/Hero.jsx
+++ src/pages/Home/sections/Hero/Hero.jsx
@@ -1,15 +1,19 @@
 import styles from "./Hero.module.css";
 import Avatar from "../../../../assets/images/avatar.png";
-import Container from "../../../../components/container/Container.jsx";
 
 function Hero() {
   return (
-      <div className={styles.Main}>
-        <div className={styles.img_Content}>
-          <img src={Avatar} alt="Avatar do usuário" className={styles.Avatar} />
-        </div>
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Tenetur odit cupiditate optio asperiores assumenda natus architecto, et quidem obcaecati commodi aut in. Autem aut laborum velit animi nulla deleniti temporibus.</p>
+    <div className={styles.Main}>
+      <div className={styles.img_Content}>
+        <img src={Avatar} alt="Avatar do usuário" className={styles.Avatar} />
       </div>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Tenetur odit
+        cupiditate optio asperiores assumenda natus architecto, et quidem
+        obcaecati commodi aut in. Autem aut laborum velit animi nulla deleniti
+        temporibus.
+      </p>
+    </div>
   );
 }
 
